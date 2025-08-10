73f38e5543447aef6c4bc0c11f5e63308b75537a
bunnysmains-bit <bunnysmains@gmail.com>
2025-08-10 01:23:51 +0100

    multiple fixed
    added mulitple new links to scrape fixed out error and code structure

--- http.go
+++ http.go
@@ -5,66 +5,72 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
-	urlproxy := "http://free-proxy-list.net/"
-
-	req, err := http.NewRequest("GET", urlproxy, nil)
-	if err != nil {
-		fmt.Println("\nError while trying to create a request:", err)
-		return
+	urls := []string{
+		"https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+		"https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+		"https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all",
+		"https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-https.txt",
+		"https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-http.txt",
+		"https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+		"https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+		"https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+		"https://www.proxy-list.download/api/v1/get?type=http",
+		"https://www.proxy-list.download/api/v1/get?type=https",
+		"https://www.proxyscan.io/download?type=http",
+		"https://www.proxyscan.io/download?type=https",
+		"https://api.openproxylist.xyz/http.txt",
+		"http://ab57.ru/downloads/proxyold.txt",
+		"http://alexa.lr2b.com/proxylist.txt",
+		"http://rootjazz.com/proxies/proxies.txt",
+		"http://spys.me/proxy.txt",
+		"http://worm.rip/http.txt",
+		"http://www.httptunnel.ge/ProxyListForFree.aspx",
+		"http://www.proxyserverlist24.top/feeds/posts/default",
+		"https://api.proxyscrape.com/?request=displayproxies&proxytype=http",
+		"https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=6000&country=all&ssl=yes&anonymity=all",
+		"https://dstat.one/proxies.php?id=2",
+		"https://dstat.one/proxies.php?id=3",
+		"https://free-proxy-list.net/",
+		"https://free-proxy-list.net/anonymous-proxy.html",
+		"https://free-proxy-list.net/uk-proxy.html",
+		"https://www.proxy-list.download/api/v1/get?type=http",
+		"https://www.proxy-list.download/api/v1/get?type=https",
+		"https://www.proxyscan.io/download?type=http",
+		"https://www.proxyscan.io/download?type=https",
 	}
-
-	req.Header.Add("User-Agent", "Mozilla/4.0 (PSP (PlayStation Portable); 2.00)")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("\nError while trying to send a request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("\nError: Unexpected status code", resp.StatusCode)
-		return
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("\nError while trying to read response body:", err)
-		return
-	}
-
-	pageContent := string(body)
-
-	startIndex := strings.Index(pageContent, "<tbody>")
-	if startIndex == -1 {
-		fmt.Println("\nError: <tbody> tag not found in the response")
-		return
-	}
-
-	endIndex := strings.Index(pageContent, "</tbody>")
-	if endIndex == -1 {
-		fmt.Println("\nError: </tbody> tag not found in the response")
-		return
-	}
-
-	tbodyContent := pageContent[startIndex+len("<tbody>") : endIndex]
-
-	rows := strings.Split(tbodyContent, "<tr><td>")
 
 	proxies := ""
-	for _, row := range rows {
-		cells := strings.Split(row, "</td><td>")
-		if len(cells) >= 2 {
-			proxies += cells[0] + ":" + cells[1] + "\n"
+	client := &http.Client{}
+	for _, url := range urls {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Println("\nError creating request for", url, ":", err)
+			continue
 		}
+		req.Header.Add("User-Agent", "Mozilla/4.0 (PSP (PlayStation Portable); 2.00)")
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("\nError fetching", url, ":", err)
+			continue
+		}
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("\nError: Unexpected status code", resp.StatusCode, "for", url)
+			resp.Body.Close()
+			continue
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Println("\nError reading response from", url, ":", err)
+			continue
+		}
+		proxies += string(body) + "\n"
 	}
 
-	outFile, err := os.Create("http.txt")
+	outFile, err := os.Create("proxies/http.txt")
 	if err != nil {
 		fmt.Println("\nError while creating 'http.txt':", err)
 		return
